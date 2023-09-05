b0762d6a4b844cdaadd9ed2ab35ff1dca8c95690
Keri <kerison913@gmail.com>
2023-09-05 10:50:15 +0900

    started incorporating dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -24,27 +24,33 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
+      themeMode: ThemeMode.system,
       theme: ThemeData(
-        splashColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFFe9435a),
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Color(0xFFe9435a),
-        ),
-        appBarTheme: const AppBarTheme(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
+          brightness: Brightness.light,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: const Color(0xFFe9435a),
+          textSelectionTheme: const TextSelectionThemeData(
+            cursorColor: Color(0xFFe9435a),
           ),
-        ),
-      ),
+          appBarTheme: const AppBarTheme(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: Sizes.size16 + Sizes.size2,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          bottomAppBarTheme: BottomAppBarTheme(
+            color: Colors.grey.shade50,
+          )),
       darkTheme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
+        primaryColor: const Color(0xFFe9435a),
+        brightness: Brightness.dark,
       ),
       home: const SignUpScreen(),
     );
